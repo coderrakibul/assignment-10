866e2b6331b57f7668ed5d94c5ee6ebfe5991802
coderrakibul <coder.rakibul@gmail.com>
2022-04-17 03:03:32 +0600

    service section is ready

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import banner from '../images/banner.webp';
+import Service from '../Service/Service';
+import './Home.css'
+
 
 const Home = () => {
+    const [services, setServices] = useState([]);
+    useEffect( () => {
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setServices(data));
+    }, []);
+
     return (
-        <div className='container'>
-            <h2>this is home page</h2>
+        <div className='mb-5'>
+            <img className='w-100 banner-img mb-5' src={banner} alt="" />
+
+            <h2 className='text-center mb-4 mt-5'>My Services</h2>
+            <div className='container service-container'>
+            {
+                services.map(service => <Service 
+                    key={service.id}
+                    service={service}
+                    ></Service>)
+            }
+            </div>
         </div>
     );
 };
---
+++ src/Service/Service.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import './Service.css'
+
+const Service = (props) => {
+    const { name, price, description, image } = props.service
+    return (
+        <div>
+            <Card className='service-card shadow p-3' style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={image} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Title>${price}</Card.Title>
+                    <Card.Text className='mb-5'>
+                        {description}
+                    </Card.Text>
+                    <Button className='checkout-btn' variant="primary">Checkout</Button>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
+
+export default Service;
