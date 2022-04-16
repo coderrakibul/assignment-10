b6eb33dd3ba76014533ea27ad11ef4bf45e4208a
coderrakibul <coder.rakibul@gmail.com>
2022-04-17 00:52:12 +0600

    header created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header> </Header>
+      <Routes>
 
+      </Routes>
     </div>
   );
 }
---
+++ src/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import './Header.css';
+
+
+const Header = () => {
+    return (
+        <>
+
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home"> <h2>Dr. John</h2></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#blogs">Blogs</Nav.Link>
+                            <Nav.Link href="#about">About</Nav.Link>
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+        </>
+    );
+};
+
+export default Header;
