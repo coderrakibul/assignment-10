bd458f0f63d2c553876663a1dabbce9aeac70f75
coderrakibul <coder.rakibul@gmail.com>
2022-04-18 13:54:16 +0600

    email verify system implemented

--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import './Checkout.css'
+import React, { useState } from 'react';
+import './Checkout.css';
+import auth from '../firebase.init';
+import { useSendEmailVerification } from 'react-firebase-hooks/auth';
 
 const Checkout = () => {
+    const [email, setEmail] = useState('');
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(
+        auth
+    );
+
+    if (error) {
+        return (
+            <div>
+                <p>Error: {error.message}</p>
+            </div>
+        );
+    }
+    if (sending) {
+        return <p>Sending...</p>;
+    }
     return (
         <div className='container text-center checkout-container mt-5'>
-            <h1>This is Checkout Page</h1>
+            <h1>This is checkout page</h1>
+            <button className='btn btn-primary'
+                onClick={async () => {
+                    await sendEmailVerification();
+                    alert('Sent email');
+                }}
+            >
+                Please verify your email
+            </button>
         </div>
     );
 };
